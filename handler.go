e75856f3a8bade3a718d6b155a400c001b3baf51
eiixy <990656271@qq.com>
2025-01-17 14:04:33 +0800

    feat: update file type

--- handler.go
+++ handler.go
@@ -1,10 +1,7 @@
 package swagger
 
 import (
-	"bytes"
-	"embed"
 	"fmt"
-	"gopkg.in/yaml.v3"
 	"html/template"
 	"io/fs"
 	"net/http"
@@ -12,6 +9,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-keg/swagger-api/dist"
+	"gopkg.in/yaml.v3"
 )
 
 const IndexTemp = `<!-- HTML for static distribution bundle build -->
@@ -101,7 +99,7 @@ type Openapi struct {
 }
 
 // Handler swagger ui
-func Handler(apis embed.FS, opts ...Option) http.Handler {
+func Handler(apis fs.FS, opts ...Option) http.Handler {
 	cfg := config{
 		prefix:        "swagger",
 		swaggerUIPath: "ui",
@@ -117,12 +115,12 @@ func Handler(apis embed.FS, opts ...Option) http.Handler {
 				return err
 			}
 			if !d.IsDir() {
-				content, err := apis.ReadFile(filePath)
+				content, err := apis.Open(filePath)
 				if err != nil {
 					return err
 				}
 				var openapi Openapi
-				decoder := yaml.NewDecoder(bytes.NewReader(content))
+				decoder := yaml.NewDecoder(content)
 				if err := decoder.Decode(&openapi); err != nil {
 					panic(fmt.Errorf("解析配置文件失败: %v", err))
 				}
